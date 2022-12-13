bc0e295d9eecac276a14fdd5078d2b2335c9771d
Gayrat Vlasov <ggvlasov@gmail.com>
2022-12-13 12:22:37 +0300

    10 Виртуализация. Собственный Eslint плагин. Анализ AST дерева. Доступность и семантика. Кастомные dropdowns
    63 Оптимизация больших списков. Виртуализация

---
+++ src/entities/Article/model/types/article.ts
@@ -0,0 +1,61 @@
+import { User } from 'entities/User';
+
+export enum ArticleSortField {
+    VIEWS = 'views',
+    TITLE = 'title',
+    CREATED = 'createdAt',
+}
+
+export enum ArticleBlockType {
+    CODE = 'CODE',
+    IMAGE = 'IMAGE',
+    TEXT = 'TEXT',
+}
+
+export interface ArticleBlockBase {
+    id: string;
+    type: ArticleBlockType;
+}
+
+export interface ArticleCodeBlock extends ArticleBlockBase {
+    type: ArticleBlockType.CODE;
+    code: string;
+}
+
+export interface ArticleImageBlock extends ArticleBlockBase {
+    type: ArticleBlockType.IMAGE;
+    src: string;
+    title: string;
+}
+
+export interface ArticleTextBlock extends ArticleBlockBase {
+    type: ArticleBlockType.TEXT;
+    paragraphs: string[];
+    title?: string;
+}
+
+export type ArticleBlock = ArticleCodeBlock | ArticleImageBlock | ArticleTextBlock;
+
+export enum ArticleType {
+    ALL = 'ALL',
+    IT = 'IT',
+    SCIENCE = 'SCIENCE',
+    ECONOMICS = 'ECONOMICS'
+}
+
+export enum ArticleView {
+    BIG = 'BIG',
+    SMALL = 'SMALL',
+}
+
+export interface Article {
+    id: string;
+    title: string;
+    user: User;
+    subtitle: string;
+    img: string;
+    views: number;
+    createdAt: string;
+    type: ArticleType[];
+    blocks: ArticleBlock[];
+}
---
+++ src/entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -0,0 +1,45 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
+import { Text } from 'shared/ui/Text/Text';
+import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
+import { AppLink } from 'shared/ui/AppLink/AppLink';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import cls from './CommentCard.module.scss';
+import { Comment } from '../../model/types/comment';
+
+interface CommentCardProps {
+    className?: string;
+    comment?: Comment;
+    isLoading?: boolean;
+}
+
+export const CommentCard = memo((props: CommentCardProps) => {
+    const { className, comment, isLoading } = props;
+
+    if (isLoading) {
+        return (
+            <div className={classNames(cls.CommentCard, {}, [className, cls.loading])}>
+                <div className={cls.header}>
+                    <Skeleton width={30} height={30} border="50%" />
+                    <Skeleton height={16} width={100} className={cls.username} />
+                </div>
+                <Skeleton className={cls.text} width="100%" height={50} />
+            </div>
+        );
+    }
+
+    if (!comment) {
+        return null;
+    }
+
+    return (
+        <div className={classNames(cls.CommentCard, {}, [className])}>
+            <AppLink to={`${RoutePath.profile}${comment.user.id}`} className={cls.header}>
+                {comment.user.avatar ? <Avatar size={30} src={comment.user.avatar} /> : null}
+                <Text className={cls.username} title={comment.user.username} />
+            </AppLink>
+            <Text className={cls.text} text={comment.text} />
+        </div>
+    );
+});
