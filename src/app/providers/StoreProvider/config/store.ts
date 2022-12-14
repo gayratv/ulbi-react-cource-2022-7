04f9e501c7abefca5f7264fdee7aca5d129377e5
Gayrat Vlasov <ggvlasov@gmail.com>
2022-12-14 11:12:37 +0300

    10 Виртуализация. Собственный Eslint плагин. Анализ AST дерева. Доступность и семантика. Кастомные dropdowns
    65 Позиционирование элементов. Отступы по дизайн системе

---
+++ src/app/providers/StoreProvider/config/store.ts
@@ -0,0 +1,44 @@
+import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
+import { counterReducer } from 'entities/Counter';
+import { userReducer } from 'entities/User';
+import { $api } from 'shared/api/api';
+import { CombinedState, Reducer } from 'redux';
+import { uiReducer } from 'features/UI';
+import { StateSchema, ThunkExtraArg } from './StateSchema';
+import { createReducerManager } from './reducerManager';
+
+export function createReduxStore(
+    initialState?: StateSchema,
+    asyncReducers?: ReducersMapObject<StateSchema>,
+) {
+    const rootReducers: ReducersMapObject<StateSchema> = {
+        ...asyncReducers,
+        counter: counterReducer,
+        user: userReducer,
+        ui: uiReducer,
+    };
+
+    const reducerManager = createReducerManager(rootReducers);
+
+    const extraArg: ThunkExtraArg = {
+        api: $api,
+    };
+
+    const store = configureStore({
+        reducer: reducerManager.reduce as Reducer<CombinedState<StateSchema>>,
+        devTools: __IS_DEV__,
+        preloadedState: initialState,
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+            thunk: {
+                extraArgument: extraArg,
+            },
+        }),
+    });
+
+    // @ts-ignore
+    store.reducerManager = reducerManager;
+
+    return store;
+}
+
+export type AppDispatch = ReturnType<typeof createReduxStore>['dispatch'];
