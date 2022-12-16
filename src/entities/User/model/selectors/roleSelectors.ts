3441c816b68cfbcab547aae0bc04b95a64b8926c
Gayrat Vlasov <ggvlasov@gmail.com>
2022-12-17 02:45:08 +0300

    11 Генератор слоев. RTK query. Начало большого рефакторинга. Роли пользователя. 18 реакт. Babel loader. Кольцевые зависимости.
    73 Роли пользователя. Доступ по ролям. Forbidden page
    isUserHaveRole

--- src/entities/User/model/selectors/roleSelectors.ts
+++ src/entities/User/model/selectors/roleSelectors.ts
@@ -6,3 +6,6 @@ export const getUserRoles = (state: StateSchema) => state.user.authData?.roles;
 
 export const isUserAdmin = createSelector(getUserRoles, (roles) => Boolean(roles?.includes(UserRole.ADMIN)));
 export const isUserManager = createSelector(getUserRoles, (roles) => Boolean(roles?.includes(UserRole.MANAGER)));
+export const isUserHaveRole = createSelector([getUserRoles,
+    (_, role : UserRole) => role,
+], (roles, role) => Boolean(roles?.includes(role)));
