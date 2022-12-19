bd2b33388da4a293e29edddf2edf15f9f1618892
Gayrat Vlasov <ggvlasov@gmail.com>
2022-12-20 00:00:37 +0300

    87 Имплементация оценки для статьи. RTK mutations, queries
    12 Popover. Drawer. Разные user agents. Алиасы. Автоматизированный рефакторинг. Парсинг AST. BrowserList. Vite. Оценка статьи

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -11,16 +11,21 @@ interface CardProps extends HTMLAttributes<HTMLDivElement> {
     className?: string;
     children: ReactNode;
     theme?: CardTheme;
+    max?: boolean;
 }
 
 export const Card = memo((props: CardProps) => {
     const {
-        className, children, theme = CardTheme.NORMAL, ...otherProps
+        className,
+        children,
+        theme = CardTheme.NORMAL,
+        max,
+        ...otherProps
     } = props;
 
     return (
         <div
-            className={classNames(cls.Card, {}, [className, cls[theme]])}
+            className={classNames(cls.Card, { [cls.max]: max }, [className, cls[theme]])}
             {...otherProps}
         >
             {children}
