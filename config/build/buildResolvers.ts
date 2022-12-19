74a80d58a127eaa894d7b7639b0080e00f522675
Gayrat Vlasov <ggvlasov@gmail.com>
2022-12-19 18:27:14 +0300

    85 Настраиваем Vite. Быстрая сборка для dev
    12 Popover. Drawer. Разные user agents. Алиасы. Автоматизированный рефакторинг. Парсинг AST. BrowserList. Vite. Оценка статьи

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,29 +1,14 @@
 import { ResolveOptions } from 'webpack';
 import { BuildOptions } from './types/config';
 
-/*
-resolve: buildResolvers(options),
-
-https://webpack.js.org/configuration/resolve/
-
- */
 export function buildResolvers(options: BuildOptions): ResolveOptions {
     return {
         extensions: ['.tsx', '.ts', '.js'],
         preferAbsolute: true,
-
-        /*
-            Modules are searched for inside all directories specified in resolve.modules
-         */
         modules: [options.paths.src, 'node_modules'],
         mainFiles: ['index'],
         alias: {
-            '@': options.paths.src, // path.resolve(__dirname, 'src'),
-
-            /*
-            Utilities: path.resolve(__dirname, 'src/utilities/'),
-            Templates: path.resolve(__dirname, 'src/templates/'),
-             */
+            '@': options.paths.src,
         },
     };
 }
