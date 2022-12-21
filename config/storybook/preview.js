e5a6b5da589ffab80995d226054f0e601e655ee2
Gayrat Vlasov <ggvlasov@gmail.com>
2022-12-21 13:16:37 +0300

    100 Generic components
    13 Арх-ые правила eslint. Автофикс. Test public api. Рефакторинг. Документация. Сборка. Улучшаем работу со store

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -16,6 +16,14 @@ export const parameters = {
         },
     },
     layout: 'fullscreen',
+    themes: {
+        default: 'light',
+        list: [
+            { name: 'light', class: Theme.LIGHT, color: '#ffffff' },
+            { name: 'dark', class: Theme.DARK, color: '#000000' },
+            { name: 'orange', class: Theme.ORANGE, color: '#ffb005' },
+        ],
+    },
 };
 
 addDecorator(StyleDecorator);
